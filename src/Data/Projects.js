b919d384cee9fad6a855ac67fa18a536a03b81b0
youssef7474 <youssef.mohamed.refai@gmail.com>
2024-01-30 17:14:55 +0200

    projects section

---
+++ src/Data/Projects.js
@@ -0,0 +1,97 @@
+import project1 from "../assets/imges/projects/Screenshot (122).png"
+import project3 from "../assets/imges/projects/Screenshot (124).png"
+import project4 from "../assets/imges/projects/Screenshot (125).png"
+import project5 from "../assets/imges/projects/Screenshot (126).png"
+import project6 from "../assets/imges/projects/Screenshot (127).png"
+import projectSome from "../assets/imges/projects/some.avif"
+import project7 from "../assets/imges/projects/Screenshot (129).png"
+import TypeScript from "../assets/imges/projects/TypeScript.jpg"
+
+export const  Projects =[
+    {
+        title: "Magna Dashboard",
+        description: "Graduation project build with react and redux toolkit bootstarp and firebase for wireless patient monitoring system",
+        imgUrl: project1,
+        demoLink:"https://admin-dashboard-youssef.netlify.app/",
+        gitHubLink:"https://github.com/youssef7474/Graduation-project"
+      },
+      {
+        title: "E-commerce store",
+        description: "Build with raect js Api and Tailwind css",
+        imgUrl: project3,
+        demoLink:"https://e-commerce-youssef.netlify.app/",
+        gitHubLink:"https://github.com/youssef7474/ecommerce-shop"
+      },
+      {
+        title: "weather app",
+        description: "Build with React js openWeather Api TailWind css",
+        imgUrl: project4,
+        demoLink:"https://weather-app-youssef.netlify.app/",
+        gitHubLink:"https://github.com/youssef7474/weather/tree/main/hi"
+      },
+      {
+        title: "resturan menu",
+        description: "Build with react js and bootstrap",
+        imgUrl: project5,
+        demoLink:"https://resturan-menu-youssef.netlify.app/",
+        gitHubLink:"https://github.com/youssef7474/Menu"
+      },
+      {
+        title: "Movies webSite",
+        description: "Build with React js Bootstrap and Api",
+        imgUrl: project6,
+        demoLink:"https://youssefmovies.netlify.app/",
+        gitHubLink:"https://github.com/youssef7474/Movies"
+      },
+      
+]
+
+export const  ProjectsCode =[
+    {
+        title: "Curd book store system app",
+        description: "Built with React JS, Redux Toolkit, Bootstrap, and a JSON API",
+        imgUrl: projectSome,
+      },
+      {
+        title: "Dates-Reminder",
+        description: "Dates Reminder with react js",
+        imgUrl: projectSome,
+        gitHubLink:"https://github.com/youssef7474/Dates-Reminder"
+      }
+      ,
+      {
+        title: "QA",
+        description: "QA with react js and localStorage",
+        imgUrl: projectSome,
+        gitHubLink:"https://github.com/youssef7474/QA"
+      },
+      {
+        title: "SomeLanding Pages ",
+        description: "Some Landing pages with Html Css And Bootstarp",
+        imgUrl: projectSome,
+        gitHubLink:"https://github.com/youssef7474/landingpages"
+      },
+]
+
+export const  ProjectsTypeScript =[
+    {
+        title: "HangMan game TypeScript",
+        description: "HangMan game build with React with typeScript Bootstarp And Vite ",
+        imgUrl: project7,
+        demoLink:"https://hangman-youssef.netlify.app/",
+        gitHubLink:"https://github.com/youssef7474/HangMan-TypeScript"
+    },
+    {
+        title: "TypeScript shopping card",
+        description: "Typescript shooping card build with react typescript Bootstarp and vite",
+        imgUrl: TypeScript,
+        gitHubLink:"https://github.com/youssef7474/typescript-card"
+    }
+    ,
+    {
+        title: "TypeScript Note App",
+        description: "Typescript NoteApp build with react typescript Bootstarp and vite",
+        imgUrl: TypeScript,
+        gitHubLink:"https://github.com/youssef7474/TypeScriptNote"
+    }
+]
---
+++ src/components/ProjectsCompo/Compounents/ProjectCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Col } from 'react-bootstrap'
+import { FaGithub } from "react-icons/fa";
+import { GrDeploy } from "react-icons/gr";
+
+const ProjectCard = ({imgUrl,title,description,demoLink,gitHubLink}) => {
+    const navigateDemo=()=>{
+        window.open(demoLink, '_blank');
+    }
+    const navigateGithub=()=>{
+        window.open(gitHubLink, '_blank');
+    }
+  return (
+    <Col sm={6} md={4}>
+        <div className='proj-imgbx'>
+            <img src={imgUrl} alt='p'></img>
+            <div className='proj-txtx'>
+                <h4>{title}</h4>
+                <span>{description}</span>
+                <div style={{display:"flex", alignItems:"center",justifyContent:"center",gap:"10px",marginTop:"15px"}}>
+                    {
+                        demoLink&&(
+                            <GrDeploy size={25} style={{cursor:"pointer"}} onClick={navigateDemo}/>
+                        )
+                    }
+                    {
+                        gitHubLink&&(
+                            <FaGithub style={{cursor:"pointer"}} size={25} onClick={navigateGithub}/>
+                        )
+                    }
+                </div>
+            </div>
+            
+        </div>
+    </Col>
+  )
+}
+
+export default ProjectCard
